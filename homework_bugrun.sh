5a5b41f9eec5244c85dec799ceda3a38fb3a7f32
Owen Kwok <okwok1@jhu.edu>
2014-09-03 12:34:31 -0400

    changed homework_bugrun.sh and run the complete trio

--- homework_bugrun.sh
+++ homework_bugrun.sh
@@ -11,31 +11,18 @@ OUTPUT_DIR=/Users/cmdb/data/day1
 SAMPLE_PREFIX=SRR072 #assigned SRR072 to SAMPLE_PREFIX
 
 GENOME_DIR=/Users/cmdb/data/genomes
-GENOME=dmel-all-chromosome-r5 #assigned dmel5 to X
+GENOME=dmel-all-chromosome-r5.57 #assigned dmel5 to GENOME
 ANNOTATION=dmel-all-r5.57.gff
 
-CLOUT=_clout
-
-#WHat's this?
+#This is for -p
 CORES=4
 
-#File names
-ONE=893
-TWO=903
-THREE=905
-FOUR=915
-FIVE=
-SIX=
-SEVEN=
-EIGHT=
-
-
 #added what i is equal to $ONE
-for i in 893
+for i in {893..916}
 do
-  FastQC $FASTQ_DIR/$SAMPLE_PREFIX$i\.fastq.gz -o $OUTPUT_DIR
-  TopHat -p1 -G -o $OUTPUT_DIR $GENOME_DIR/$GENOME\.57 $GENOME_DIR/$GENOME\.57.fasta
-  CuffLinks -G /Users/cmdb/data/dmel_r5.57_FB2014_03/gff/$ANNOTATION
+  echo FastQC $FASTQ_DIR/$SAMPLE_PREFIX$i\.fastq.gz -o $OUTPUT_DIR
+  echo TopHat -p$CORES -G $OUTPUT_DIR/gff/$ANNOTATION -o $OUTPUT_DIR/$SAMPLEPREFIX$i\_tophatout --no-novel-juncs --segment-length 20 $GENOME_DIR/$GENOME $FASTQ_DIR/$SAMPLE_PREFIX$i\.fastq.gz
+  echo CuffLinks -p$CORES -G $OUTPUD_DIR/gff/$ANNOTATION -o $OUTPUT_DIR/cl-$i $OUTPUT_DIR/$SAMPLE_PREFIX$i\_tophatout/accepted_hits.bam
 done
 #added the do and done
 #removed echo so that the programs can now run
@@ -47,3 +34,4 @@ done
 #not sure what are the correct inputs for TopHat and CuffLinks; FastQC is the only one working properly
 #unsure what genome index base is supposed to mean in: tophat [options]* <genome_index_base> <reads1_1[,...,readsN_1]> [reads1_2,...readsN_2] 
 #can't figure out which file to use for CuffLinks
+#reconfigured TopHat and CuffLinks
