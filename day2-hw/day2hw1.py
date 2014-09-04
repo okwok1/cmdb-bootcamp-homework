396c5f289fb139138c35501345778c53e0bc4a8b
Owen Kwok <okwok1@jhu.edu>
2014-09-04 18:20:17 -0400

    changed some directories

--- day2-hw/day2hw1.py
+++ day2-hw/day2hw1.py
@@ -13,10 +13,31 @@ dfrange = dfmax - dfmin
 cufflinks_output2 = "/Users/cmdb/data/results/SRR072915_clout/genes.fpkm_tracking"
 df2 = pd.read_table( cufflinks_output2 )
 
-df2median = df2['FPKM'].median()
-df2max = df2['FPKM'].max()
-df2min = df2['FPKM'].min()
-df2range = df2max - df2min
+#print df.describe(percentiles=[0.33, 0.66])["FPKM"]
+#print df2.describe(percentiles=[0.33, 0.66])["FPKM"]
+
+#df_33ile = 0
+#df_66ile = 16.637064
+#df2_33ile = 0.059101
+#df2_66ile = 10.313248
+
+#male_bottom = df[ df["FPKM"] < df_33ile]
+#male_middle = df[ df[df["FPKM"] < df_66ile ] >= df_33ile]
+#male_top = df[ df["FPKM"] >= df_66ile]
+#female_bottom = df2[ df2["FPKM"] < df2_33ile]
+#female_middle = df2[ df2[df2["FPKM"] < df2_66ile] >= df2_33ile]
+#female_top = df2[ df2["FPKM"] >= df2_66ile]
+sortM = df.sort("FPKM", ascending=True)
+sortF = df2.sort("FPKM", ascending=True)
+
+bottom_male = sortM[0:5196]["FPKM"]
+middle_male = sortM[5196:10390]["FPKM"]
+top_male = sortM[10390:15603]["FPKM"]
+bottom_female = sortF[0:5196]["FPKM"]
+middle_female = sortF[5196:10390]["FPKM"]
+top_female = sortF[10390:15603]["FPKM"]
+
+boxplot_data = [bottom_male, middle_male, top_male, bottom_female, middle_female, top_female]
 
 import matplotlib.pyplot as plt
 
@@ -24,27 +45,12 @@ import matplotlib.pyplot as plt
 #Make only one box plot: for FPKMs
 #figure out how to make the box plots look like box plots
 plt.figure()
-plt.yscale('log')
 
-plt.xlabel('male cycle 10')
-plt.ylabel('FPKM')
-plt.axis([1,5,0.01,100000])
-plt.title('male cycle 10')
-
-plt.boxplot(df['FPKM'], 0)
-
+#plt.ylabel('FPKM')
+#plt.axis([0,7,0,220])
+plt.ylim(-10,100)
+#plt.title('')
+plt.boxplot( boxplot_data )
 plt.savefig("boxplot1.png")
-
-plt.figure()
-plt.yscale('log')
-
-plt.xlabel('female cycle 14')
-plt.ylabel('FPKM')
-plt.axis([1,5,1,100000])
-plt.title('female cycle 14')
-
-plt.boxplot(df2['FPKM'], 0)
-
-plt.savefig("boxplot2.png")
 
 #These box plots look funny.
---
+++ day3-lunch/parse_blast.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+
+"""
+Determine the Blast output by printing the sequence name, ratio of identities, and ratio of gaps
+"""
+
+import sys #core utilities; stdin stdout
+
+line = sys.stdin.readline()
+assert line.startswith( "BLASTN" ) #this is the file I would expect: a fasta file
+sid = line[1:].rstrip("\r\n") #takes all characters after 1st, strips spaces from rear/end of string
+
+#sequences = [] #create list
+List = []
+while True:
+    line = sys.stdin.readline()
+    #if line == "": #stops if the program reaches the end
+    #    break
+    #if line.startswith( "> N" ):
+    #    print line.strip("> ")
+    #else: #remove the first else so that it doesn't skip the " Identities"
+    #    continue
+    #if line.startswith( " Identities"):
+    #    print line.lstrip()
+    #elif line == "":
+    #    break
+    #else:
+    #    continue
+    if line == "": #stops if the program reaches the end
+        break
+    if line.startswith( "Query=" ):
+        List.append( line.strip("\n") )
+        List.append( "\n")
+        print line.strip("\n")
+    if line.startswith( "> N" ):
+        fields = line.split(" ")
+        name = fields[1] #takes the NM/NR name, not the number afterwards
+        List.append(name.strip("> "))
+        print name.strip("> ")
+    if line.startswith( " Identities"):
+        one = line.strip(" Identities=") #removes Identities
+        two = one.replace(" Gaps =", "") #cuts out Gaps =
+        three = two.split(" ")
+        List.append(three[0])
+        List.append(three[2])
+        List.append( "\n" )
+        #print two.lstrip()
+        print three[0], three[2] #prints out only the ratios, not the percentages
+    elif line == "":
+        break
+    else:
+        continue    
+
+Lists = " ".join(List)
+print Lists
+#sequence = "".join( sequences ) #join together the list using this string as a delimiter; so pushes everything together; not add to string
+
+#print sid, sequence #ID, then sequence
